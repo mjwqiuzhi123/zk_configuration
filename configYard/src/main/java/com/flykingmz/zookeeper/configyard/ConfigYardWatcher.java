286807296f42e723be985a1071315e0e2246cac5
mjw <905396838@qq.com>
2018-04-12 18:16:06 +0800

    分布式配置中心

---
+++ configYard/src/main/java/com/flykingmz/zookeeper/configyard/ConfigYardWatcher.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package com.flykingmz.zookeeper.configyard;
+
+import java.util.List;
+
+import org.I0Itec.zkclient.IZkChildListener;
+import org.I0Itec.zkclient.IZkDataListener;
+import org.I0Itec.zkclient.ZkClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author flyking
+ * 
+ */
+public class ConfigYardWatcher {
+	private final static Logger logger = LoggerFactory
+			.getLogger(ConfigYardWatcher.class);
+	
+	private ZkClient client;
+	
+	private ConfigYardListener configYardListener;
+	
+	private ConfigYard configYard;
+
+	public ConfigYardWatcher(ZkClient client,ConfigYard configYard) {
+		this.client = client;
+		this.configYard = configYard;
+		this.initConfigYard();
+	}
+	
+	private void initConfigYard(){
+		configYardListener = new ConfigYardListener();
+	}  
+	
+	// 每个节点共用一个监听器(单例)
+	public void watcher(String key){
+		client.subscribeDataChanges(key, configYardListener);
+		client.subscribeChildChanges(key, configYardListener);
+	}
+	
+	// 每个节点都有一个监听器
+	public void watcher(String key, ConfigYardListener configYardListener){
+		client.subscribeDataChanges(key, configYardListener);
+		client.subscribeChildChanges(key, configYardListener);
+	}
+
+	/**
+	 * 配置监听器
+	 * @author flyking
+	 *
+	 */
+	class ConfigYardListener implements IZkDataListener,IZkChildListener{
+		public void handleDataChange(String dataPath, Object data)
+				throws Exception {
+			logger.info("data {} change,start reload configProperties",dataPath);
+			configYard.reload();
+		}
+
+		public void handleDataDeleted(String dataPath) throws Exception {
+			logger.info("data {} delete,start reload configProperties",dataPath);
+			configYard.reload();
+		}
+
+		public void handleChildChange(String parentPath,
+				List<String> currentChilds) throws Exception {
+			logger.info("data {} ChildChange,start reload configProperties",parentPath);
+			configYard.reload();
+		}
+		
+	}
+}
